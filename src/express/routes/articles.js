c4cd0b497fe58ee8c83f9c288b5a1bc3f954b16b
andpost200 <andrey.chepulis@kreo.net>
2022-01-25 22:01:46 +0300

    shablon

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -5,7 +5,7 @@ const articlesRouter = new Router();
 
 
 articlesRouter.get(`/:id`, (req, res) => res.render(`articles/article`));
-articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/category`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
 articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/edit`));
 articlesRouter.get(`/add`, (req, res) => res.render(`articles/new-article`));
 
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -6,7 +6,7 @@ const myRouter = new Router();
 
 myRouter.get(`/`, (req, res) => res.render(`my/my`));
 myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
-myRouter.get(`/categories`, (req, res) => res.render(`my/categories`));
+myRouter.get(`/categories`, (req, res) => res.render(`my/all-categories`));
 
 
 module.exports = myRouter;
